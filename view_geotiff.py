f3e9b49d03a69c75c3b31614d2987090c7d38b1b
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-04-17 14:28:55 +0000

    GeoTIFF visualization added

--- view_geotiff.py
+++ view_geotiff.py
@@ -2,11 +2,11 @@ import rasterio
 import matplotlib.pyplot as plt
 
 # Open the GeoTIFF
-with rasterio.open("your_file.tif") as src:
+with rasterio.open("npp_viirs_m08_20250325_220601_wgs84_fit.tif") as src:
     data = src.read(1)  # Read the first band
     plt.imshow(data, cmap='viridis')
     plt.colorbar(label='Value')
     plt.title("GeoTIFF Visualization")
     plt.xlabel("Pixel X")
     plt.ylabel("Pixel Y")
-    plt.show()
+    plt.savefig(f"GeoTIFF_image_example", dpi=200, bbox_inches='tight')
