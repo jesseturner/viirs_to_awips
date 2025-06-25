11d9e02496c4ef5682398b88fe94a835e9a05c5e
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-06-25 19:49:54 +0000

    Fixing lock logic

--- cron1_lock_and_run.sh
+++ cron1_lock_and_run.sh
@@ -7,18 +7,21 @@ cd $working_dir
 today=$(date +%Y%m%d)
 error_time=$(date +%H%M%S)Z
 
-lock=$working_dir/lock-viirs-to-awips
+lock="$working_dir/lock-viirs-to-awips"
 
-if [ -e $lock ]
-   then
-   /bin/echo "lock exists - previous processing still running"
-        if [ /bin/find $lock -mmin +20 ]
-           then /bin/rm $lock
-        fi
-   exit 1
-else
-   /bin/touch $lock
+if [ -e "$lock" ]; then
+    echo "Lock file exists - checking age: $lock"
+    
+    if /bin/find "$lock" -mmin +20 | grep -q .; then
+        echo "Lock file is stale. Removing: $lock"
+        /bin/rm -f "$lock"
+    else
+        echo "Lock is still active. Exiting."
+        exit 1
+    fi
 fi
+
+/bin/touch "$lock"
 
 mkdir -p "$working_dir/processing_logs"
 
