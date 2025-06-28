3592eb2e6e5b9d5d0440cc600e82466ef81415b7
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-06-28 19:29:08 +0000

    Improvement on the argument logic

--- cron1-1_process_viirs_to_awips.py
+++ cron1-1_process_viirs_to_awips.py
@@ -2,28 +2,27 @@
 #------ rename to make it more clear this is the main processing code
 
 from datetime import datetime
-import argparse, glob, gzip, logging, os, shutil, subprocess, re
+import argparse, glob, gzip, logging, os, shutil, subprocess, re, sys
 
 #=====================================================
 
 def main(raw_args=None):
     
-    base_dir, recent_file_threshold, bands_to_process, sats_to_process = setUpVariables()
+    base_dir, bands_to_process, sats_to_process = setUpVariables()
     dt_info = setUpDatetimes()
     log_prefix = startLogging(base_dir, dt_info)
-    bands_to_process, dt_info, args = parseArguments(raw_args, recent_file_threshold, bands_to_process, log_prefix, dt_info)
+    bands_to_process, sats_to_process, orbits_to_process, dt_info = parseArguments(raw_args, bands_to_process, sats_to_process, log_prefix, dt_info)
     dtstamp_dir, final_dir = createTempAndOutputDir(base_dir, dt_info)
-    processingAllViirsData(dt_info, sats_to_process, bands_to_process, args, dtstamp_dir, log_prefix, base_dir, final_dir)
+    processingAllViirsData(bands_to_process, sats_to_process, orbits_to_process, dt_info, dtstamp_dir, log_prefix, base_dir, final_dir)
     finishAndClean(dtstamp_dir, log_prefix)
 
 #=====================================================
 
 def setUpVariables():
     base_dir = os.getcwd()
-    recent_file_threshold = 30 * 60 #--- time range to process, files from most recent minutes
     bands_to_process = ['m', 'i']
     sats_to_process = ['NPP', 'J01', 'J02']
-    return base_dir, recent_file_threshold, bands_to_process, sats_to_process
+    return base_dir, bands_to_process, sats_to_process
 
 #-----------------------------------------------------
 
@@ -32,16 +31,19 @@ def setUpDatetimes():
     year = current_dt.year
     month = current_dt.month
     day = current_dt.day
+    hour = current_dt.hour
 
     #--- creating a dictionary for ease of passing variables
     dt_info = {
-    "file_year": str(year),
-    "file_month": '%02d' % month,
-    "file_day": '%02d' % day,
-    "file_date": str(year) + '%02d' % month + '%02d' % day ,
-    "current_datetime": current_dt.strftime('%Y%m%d%H%M%S'),
-    "current_datetime_colons": current_dt.strftime('%Y-%m-%d %H:%M:%S'),
-    "julian_day": current_dt.timetuple().tm_yday
+        "file_year": str(year),
+        "file_month": f"{month:02d}",
+        "file_day": f"{day:02d}",
+        "file_date": f"{year}{month:02d}{day:02d}",
+        "file_hour": f"{hour:02d}",
+        "current_datetime": current_dt.strftime('%Y%m%d%H%M%S'),
+        "current_datetime_colons": current_dt.strftime('%Y-%m-%d %H:%M:%S'),
+        "julian_day": current_dt.timetuple().tm_yday,
+        "file_date_str": f"d{year}{month:02d}{day:02d}_t{hour:02d}"
     }
 
     return dt_info
@@ -60,12 +62,10 @@ def startLogging(base_dir, dt_info):
 
 #-----------------------------------------------------
 
-def parseArguments(raw_args, recent_file_threshold, bands_to_process, log_prefix, dt_info):
-    #--- checking for incoming arguments that would change processing
-    #------ arguments are in the form of a list of strings, i.e. ['-d', '20250611']
+def parseArguments(raw_args, bands_to_process, sats_to_process, log_prefix, dt_info):
+
+    #--- arguments are in the form of a list of strings, i.e. ['-d', '20250611']
     parser = argparse.ArgumentParser(description='Process incoming data from /mnt/viirs/WI-CONUS/NPP for AWIPS ingestion')
-    parser.add_argument('-t', '--time', type=int,
-                        help='Only bands with a new file in the last t seconds will be processed')
     parser.add_argument('-b', '--orbit', type=str,
                         help='Ignore the time search and only process files for orbit b (can omit the leading b)')
     parser.add_argument('-f', '--freq-band', type=str,
@@ -76,77 +76,81 @@ def parseArguments(raw_args, recent_file_threshold, bands_to_process, log_prefix
                         help='Run for the full 24 hours of a specific date (YYYYMMDD format) or for a specific hour (YYYYMMDDhh format)')
     
     args = parser.parse_args(raw_args)
-    if args.time:
-        recent_file_threshold = args.time
+
+    #--- No arguments
+    if len(sys.argv) == 1:
+        logging.info(f'{log_prefix} Looking for data from {dt_info["file_year"]}-{dt_info["file_month"]}-{dt_info["file_day"]} {dt_info["file_hour"]}:00 UTC')
+
+    #--- Specified orbit
+    if args.orbit:
+        orbits_to_process = [args.orbit if args.orbit[0] == 'b' else ('b' + args.orbit)]
+        logging.info(f'{log_prefix} Running for orbit {orbits_to_process}')
+    else: orbits_to_process = []
+
+    #--- Specified band
     if args.freq_band:
         if args.freq_band not in bands_to_process:
             logging.error('Invalid single-band input; valid options are "i" and "m"')
             exit(1)
         bands_to_process = [args.freq_band]
+
+    #--- Specified satellite
     if args.satellite:
         if args.satellite not in sats_to_process:
             logging.error('Invalid satellite input; valid options are "NPP", "J01", and "J02"')
             exit(1)
         sats_to_process = [args.satellite]
 
-    dt_info = checkForDateArgument(args, log_prefix, recent_file_threshold, dt_info)
-    
-    return bands_to_process, dt_info, args
-
-#-----------------------------------------------------
-
-def checkForDateArgument(args, log_prefix, recent_file_threshold, dt_info):
-
+    #--- Specified date
     if args.file_date:
-        dt_info['file_year'] = str(args.file_date[0:3 + 1])
-        dt_info['file_month'] = str(args.file_date[4:5 + 1])
-        dt_info['file_day'] = str(args.file_date[6:7 + 1])
-    
-    dt_info['julian_day'] = datetime(int(dt_info['file_year']), int(dt_info['file_month']), int(dt_info['file_day'])).timetuple().tm_yday
+        dt_info['file_year'] = args.file_date[:4]
+        dt_info['file_month'] = args.file_date[4:6]
+        dt_info['file_day'] = args.file_date[6:8]
+        dt_info['julian_day'] = datetime(int(dt_info['file_year']), int(dt_info['file_month']), int(dt_info['file_day'])).timetuple().tm_yday
 
-    if args.file_date:
-        if len(args.file_date) == 10:
+        #--- Depending on if hour is specified or not
+        if len(args.file_date) == 10:  #--- format: YYYYMMDDhh
+            dt_info['file_hour'] = args.file_date[8:10]
+            dt_info['file_date_str'] = f"d{dt_info['file_year']}{dt_info['file_month']}{dt_info['file_day']}_t{dt_info['file_hour']}"
             logging.info(f'{log_prefix} Looking for data from {dt_info["file_year"]}-{dt_info["file_month"]}-{dt_info["file_day"]} {args.file_date[-2:]}:00 UTC')
-        elif len(args.file_date) == 8:
+        elif len(args.file_date) == 8:  #--- format: YYYYMMDD
+            dt_info['file_date_str'] = f"d{dt_info['file_year']}{dt_info['file_month']}{dt_info['file_day']}"
             logging.info(f'{log_prefix} Looking for all data from {dt_info["file_year"]}-{dt_info["file_month"]}-{dt_info["file_day"]}')
         else:
             raise ValueError("file_date must be in YYYYMMDD or YYYYMMDDhh format")
-    else: 
-        logging.info(f'{log_prefix} Checking last {recent_file_threshold/60} minutes for new files')
-
-    return dt_info
-
-#-----------------------------------------------------
-
-def createTempAndOutputDir(base_dir, dt_info):
-    #--- creating a YYYYMMDD_hhmmss dir for an isolated workspace
-    dtstamp_dir = base_dir + '/' + dt_info['current_datetime'] + '/'
-    if not os.path.exists(dtstamp_dir):
-        os.makedirs(dtstamp_dir)
-
-    #--- creating the output directories
-    final_dir = base_dir + '/viirs_awips/'
-    if not os.path.exists(final_dir):
-        os.makedirs(final_dir)
+        
     
-    return dtstamp_dir, final_dir
+    return bands_to_process, sats_to_process, orbits_to_process, dt_info
 
 #-----------------------------------------------------
 
-def processingAllViirsData(dt_info, sats_to_process, bands_to_process, args, dtstamp_dir, log_prefix, base_dir, final_dir):
+def processingAllViirsData(bands_to_process, sats_to_process, orbits_to_process, dt_info, dtstamp_dir, log_prefix, base_dir, final_dir):
     band_params, raw_sat_names = setSatellitesAndBands(dt_info['file_year'], dt_info['julian_day'])
     for sat in sats_to_process:    #--- NPP, J01, J02
         raw_sat_name = raw_sat_names[sat]
         for band in bands_to_process:   #--- m, i
+            
+            #--- set band parameters
             band_dir = band_params[band]['band_dir'].replace('_replacewithsat_', sat)
             prod_prefixes = band_params[band]['prod_prefixes']
             ldm_file_tags = band_params[band]['ldm_file_tags']
             p2g_file_tags = list(ldm_file_tags.keys())
             output_prod_name = band_params[band]['output_prod_name']
 
-            orbits = makeListOrbits(args, band_dir)
+            #--- get files that match time
+            matching_files = [
+                os.path.basename(f) for f in glob.glob(os.path.join(band_dir, f"*{dt_info['file_date_str']}*"))
+                if dt_info['file_date_str'] in f
+            ]
 
-            for orbit in orbits: 
+            #--- create orbits list from matching files
+            for filename in matching_files:  
+                orbit = filename.split('_')[5]
+                if orbit not in orbits_to_process:
+                    orbits_to_process.append(orbit)
+
+            #--- run processing
+            for orbit in orbits_to_process: 
                 datetime_str = gettingFilesFromOrbit(prod_prefixes, sat, band_dir, band, orbit)
                 processing_dir, raw_files_dir = processingViirsFiles(dtstamp_dir, sat, band, orbit, prod_prefixes, band_dir)
                 runningPolar2Grid(log_prefix, sat, band, base_dir, raw_files_dir, orbit, processing_dir)
@@ -162,44 +166,18 @@ def processingAllViirsData(dt_info, sats_to_process, bands_to_process, args, dts
 
 #-----------------------------------------------------
 
-def makeListOrbits(args, band_dir):
+def createTempAndOutputDir(base_dir, dt_info):
+    #--- creating a YYYYMMDD_hhmmss dir for an isolated workspace
+    dtstamp_dir = base_dir + '/' + dt_info['current_datetime'] + '/'
+    if not os.path.exists(dtstamp_dir):
+        os.makedirs(dtstamp_dir)
+
+    #--- creating the output directories
+    final_dir = base_dir + '/viirs_awips/'
+    if not os.path.exists(final_dir):
+        os.makedirs(final_dir)
     
-    #--- check for specified orbit
-    orbit_to_process = None
-    if args.orbit:
-        orbit_to_process = args.orbit if args.orbit[0] == 'b' else ('b' + args.orbit)
-
-    orbits = []
-
-    if orbit_to_process:
-        orbits = [orbit_to_process]
-
-    elif args.file_date:  #--- if running for specific date or date+hour
-        if len(args.file_date) == 10:  #--- format: YYYYMMDDhh
-            file_year = args.file_date[:4]
-            file_month = args.file_date[4:6]
-            file_day = args.file_date[6:8]
-            file_hour = args.file_date[8:10]
-            file_date_str = f"d{file_year}{file_month}{file_day}_t{file_hour}"
-        elif len(args.file_date) == 8:  #--- format: YYYYMMDD
-            file_year = args.file_date[:4]
-            file_month = args.file_date[4:6]
-            file_day = args.file_date[6:8]
-            file_date_str = f"d{file_year}{file_month}{file_day}"
-        else:
-            raise ValueError("file_date must be in YYYYMMDD or YYYYMMDDhh format")
-
-        matching_files = [
-            os.path.basename(f) for f in glob.glob(os.path.join(band_dir, f"*{file_date_str}*"))
-            if file_date_str in f
-        ]
-
-        for filename in matching_files:  #--- create orbits list from matching files
-            orbit = filename.split('_')[5]
-            if orbit not in orbits:
-                orbits.append(orbit)
-
-    return orbits
+    return dtstamp_dir, final_dir
 
 #-----------------------------------------------------
 
@@ -248,9 +226,11 @@ def gettingFilesFromOrbit(prod_prefixes, sat, band_dir, band, orbit):
         if orbit_not_ready:
             logging.info(f'{sat} orbit {orbit} bands are not fully filled in yet, not processing')
             continue
-    #--- logging the files used
+        
+        #--- logging the files used
+        #--- TO FIX: this is being run even when filepaths is empty 
         filepaths = glob.glob(os.path.join(band_dir, prefix + '*_' + orbit + '_*'))
-        if filepaths:
+        if filepaths: 
             filepaths.sort()  #--- sort to follow time order
             first_file = os.path.basename(filepaths[0])
             match = re.search(r'd(\d{8})_t(\d{2})(\d{2})', first_file)
@@ -259,8 +239,13 @@ def gettingFilesFromOrbit(prod_prefixes, sat, band_dir, band, orbit):
                 dt = datetime.strptime(f"{date_str}{hour}{minute}", "%Y%m%d%H%M")
                 datetime_str = dt.strftime("%Y-%m-%d %H:%M UTC")
             else:
-                datetime_str = "Unknown date/time"
-        logging.info(f'Processing {len(filepaths)} VIIRS files for {sat} orbit {orbit} {band}-band at {datetime_str}')
+                datetime_str = "Unknown datetime"
+            logging.info(f'Processing {len(filepaths)} VIIRS files for {sat} orbit {orbit} {band}-band at {datetime_str}')
+        
+        else: 
+            datetime_str = "Unknown datetime"
+            logging.info(f'No VIIRS files found for {sat} orbit {orbit} {band}-band at {datetime_str}')
+            continue
         
         return datetime_str
     
