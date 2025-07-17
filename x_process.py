1d4863fefa963da92170f588f2783fc7f8395b07
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-07-17 18:08:49 +0000

    Method for copying files to local

--- x_process.py
+++ x_process.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-import re, os
+import re, os, shutil
 
 
-class FileSelector:
+class FileGrabber:
     def __init__(self, start_time: datetime, end_time: datetime):
         self.start_time = start_time
         self.end_time = end_time
@@ -16,7 +16,8 @@ class FileSelector:
         for sat in sats_to_process:
             for band in bands_to_process:
                 band_dir =  f"/mnt/jpssnas9/WI-CONUS/{sat}/SDR-{band}Band/{self.start_time.year}/{self.start_time.timetuple().tm_yday}/"
-                filenames.extend(os.listdir(band_dir))
+                full_paths = [os.path.join(band_dir, fname) for fname in os.listdir(band_dir)]
+                filenames.extend(full_paths)
 
         return filenames
 
@@ -41,11 +42,33 @@ class FileSelector:
         bundle_ids = self._get_valid_orbits(filenames)
         return [f for f in filenames if any(f"b{bid}" in f for bid in bundle_ids)]
 
+    def copy_files_locally(self, filenames: list[str]):
+        current_dtstamp = datetime.now().strftime('d%Y%m%dt%H%M%S')
+        created_dirs = set()
 
-start_time = datetime(2025, 7, 17, 5, 0, 0)
-end_time = datetime(2025, 7, 17, 5, 30, 0)
+        for f in filenames:
+            filename_split = re.split(r'[_/]', f)
+            sat = filename_split[4]
+            band = filename_split[5]
+            orbit = filename_split[13]
 
-selector = FileSelector(start_time, end_time)
-matching_files = selector.get_files_for_valid_orbits()
-for f in matching_files:
-    print(f)
+            processing_dir = f"{current_dtstamp}_{sat}_{band}_{orbit}/"
+            raw_files_dir = os.path.join(processing_dir, 'raw_files')
+
+            #--- Only create directories once
+            if (sat, band, orbit) not in created_dirs:
+                os.makedirs(raw_files_dir, exist_ok=True)
+                created_dirs.add((sat, band, orbit))
+            
+            shutil.copy(f, raw_files_dir)
+
+    
+
+# start_time = datetime(2025, 7, 17, 5, 0, 0)
+# end_time = datetime(2025, 7, 17, 5, 30, 0)
+
+# selector = FileGrabber(start_time, end_time)
+# matching_files = selector.get_files_for_valid_orbits()
+
+# selector.copy_files_locally(matching_files)
+
