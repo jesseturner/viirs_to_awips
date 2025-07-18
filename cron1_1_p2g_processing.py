3502c138aebe95e14e395b4d8a420f22f0a7aa90
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-07-18 20:58:10 +0000

    trying out twice-daily processing

--- cron1_1_p2g_processing.py
+++ cron1_1_p2g_processing.py
@@ -1,6 +1,7 @@
 from p2g_processing_utils import TimeWindowSelector, FileGrabber, Polar2Grid_Runner
 
-tw1 = TimeWindowSelector(mode='current')
+#tw1 = TimeWindowSelector(mode='current')
+tw1 = TimeWindowSelector(mode='day', target_date='2025-07-18')
 
 start_time, end_time = tw1.get_window()
 print(f"Searching for data from {start_time.strftime('%Y-%m-%d, %H:%M:%S')} to {end_time.strftime('%H:%M:%S')}.")
