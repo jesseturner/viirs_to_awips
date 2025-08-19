a1ab9a3804871329a92929dca823c49bd1c96401
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-08-19 21:06:01 +0000

    Cleaning out old methods

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-Setting up v2a_utils and v2a_processing to run all the cron jobs together with python. Then the rest of the functions will be deleted. 
+Functions exist in `v2a_utils/`.
 
-The main processing runs every 9 minutes (cron1): 
-- checks for new viirs files in the last 12 minutes
-- processes viirs files and uses Polar2Grid to output AWIPS files
-- moves to LDM
-
-Every 18 minutes (cron2):
-- remove anything from `viirs_awips` that is over 2 hours old
-
-Every 36 minutes (cron3): 
-- email if there is a data gap longer than 10 hours
-
-Every day (cron4): 
-- clean up `to_ldm_recent` so we only retain the files from the last 7 days
+Cron runs `v2a_processing.py`.
 
--- z_old_code/TESTING/p2g_processing_utils_testing.py
+++ z_old_code/TESTING/p2g_processing_utils_testing.py
@@ -1,9 +1,8 @@
-from p2g_processing_utils import TimeWindowSelector, FileGrabber, Polar2Grid_Runner
-from datetime import datetime
+from old_code.p2g_processing_utils import TimeWindowSelector, FileGrabber, Polar2Grid_Runner
 
-#tw1 = TimeWindowSelector(mode='current')
-current_date = datetime.now().strftime('%Y-%m-%d')
-tw1 = TimeWindowSelector(mode='day', target_date=current_date)
+# tw1 = TimeWindowSelector(mode='current')
+tw1 = TimeWindowSelector(mode='hour', target_date='2025-08-05', hour=6)
+# tw1 = TimeWindowSelector(mode='day', target_date='2025-07-30')
 
 start_time, end_time = tw1.get_window()
 print(f"Searching for data from {start_time.strftime('%Y-%m-%d, %H:%M:%S')} to {end_time.strftime('%H:%M:%S')}.")
@@ -13,6 +12,7 @@ matching_files = selector.get_files_for_valid_orbits()
 print(f"Grabbed {len(matching_files)} files.")
 
 data_dirs = selector.copy_files_locally(matching_files)
+print(data_dirs)
 
 p2g = Polar2Grid_Runner(data_dirs)
 
