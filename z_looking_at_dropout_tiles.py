91bfaa1ecc58c7a189fc1ae530f2c2d19457a35c
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-09-30 19:47:08 +0000

    Looking again at dropout tile files

--- z_looking_at_dropout_tiles.py
+++ z_looking_at_dropout_tiles.py
@@ -4,9 +4,12 @@ import xarray as xr
 import numpy as np
 import io
 
-file_dropout = "to_ldm_recent/RAMMB_VIIRS_I05_20250805_0651_011.nc.gz"
-file_complete = "to_ldm_recent/RAMMB_VIIRS_I05_20250805_0832_010.nc.gz"
-file_ldm = "ldm_file/RAMMB_VIIRS_I05_20250805_0651_012.nc.gz"
+file_dropout = "3_to_ldm_recent/RAMMB_VIIRS_I05_20250930_0649_009.nc.gz"
+file_complete = "3_to_ldm_recent/RAMMB_VIIRS_I05_20250930_0649_010.nc.gz"
+#--- Using the following:
+#  scp ldm@cira-ldm1:/home/ldm/products_sent/viirs_i/I05/RAMMB_VIIRS_I05_20250930_0649_010.nc.gz .
+file_ldm = "RAMMB_VIIRS_I05_20250930_0649_010.nc.gz"
+
 
 def open_file(file):
     with gzip.open(file, 'rb') as f:
@@ -19,7 +22,5 @@ ds_complete = open_file(file_complete)
 ds_ldm = open_file(file_ldm)
 
 print(ds_dropout.data.values)
-
 print(ds_complete.data.values)
-
 print(ds_ldm.data.values)
